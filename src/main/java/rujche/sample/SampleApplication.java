38d5782ab49b4255370c104662ee4a861528bcea
rujche <rujche@microsoft.com>
2024-07-07 22:10:06 +0800

    Produce and consume message by spring cloud stream.

--- src/main/java/rujche/sample/SampleApplication.java
+++ src/main/java/rujche/sample/SampleApplication.java
@@ -1,38 +1,31 @@
 package rujche.sample;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import rujche.sample.avsc.generated.SampleMessage;
-import rujche.sample.integration.EntryGateway;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Date;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class SampleApplication {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SampleApplication.class);
 
-    public static void main(String[] args) throws IOException {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(SampleApplication.class).run(args);
-        EntryGateway entry = context.getBean(EntryGateway.class);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please input a line to send message. input 'exit' to exit.");
-        while (true) {
-            String line = reader.readLine();
-            if (line.equals("exit")) {
-                System.out.println("Closing application context, please wait a few seconds...");
-                context.close();
-                break;
-            }
-            SampleMessage message = new SampleMessage(line, "Created in SampleApplication. date = " + new Date());
-            LOGGER.info("Sending message by EntryGateway. message = {}.", line);
-            entry.sendMessage(message);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    // I tried to change type parameter from "Object" to "SimpleMessage", but failed.
+    // If you can achieve this, welcome to create a PR.
+    @Bean
+    public Consumer<Object> consumer(){
+        return message -> System.out.println("Consumed message. message = " + message);
+    }
+
+    @Bean
+    public Supplier<SampleMessage> supplier() {
+        return () -> new SampleMessage("Sample message", new Date().toString());
     }
 
 }
--- src/main/java/rujche/sample/integration/ChannelNames.java
+++
@@ -1,9 +0,0 @@
-package rujche.sample.integration;
-
-public final class ChannelNames {
-    public static final String ENTRY_CHANNEL = "entryChannel";
-    public static final String KAFKA_PRODUCER_CHANNEL = "kafkaProducerChannel";
-    public static final String KAFKA_CONSUMER_CHANNEL = "kafkaConsumerChannel";
-    public static final String LOG_CHANNEL_1 = "logChannel1";
-    public static final String LOG_CHANNEL_2 = "logChannel2";
-}
