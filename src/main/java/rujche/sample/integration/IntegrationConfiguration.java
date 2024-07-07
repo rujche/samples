9f689e16188fcae7161b41288f1b26a9519425d6
rujche <rujche@microsoft.com>
2024-07-07 14:25:42 +0800

    Add kafkaConsumerChannel.

--- src/main/java/rujche/sample/integration/IntegrationConfiguration.java
+++ src/main/java/rujche/sample/integration/IntegrationConfiguration.java
@@ -5,14 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.expression.common.LiteralExpression;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.kafka.inbound.KafkaMessageSource;
 import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
 import org.springframework.integration.router.RecipientListRouter;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ConsumerProperties;
 import org.springframework.messaging.MessageHandler;
 import rujche.sample.EventHubsProperties;
 
 import static rujche.sample.integration.ChannelNames.ENTRY_CHANNEL;
+import static rujche.sample.integration.ChannelNames.KAFKA_CONSUMER_CHANNEL;
 import static rujche.sample.integration.ChannelNames.KAFKA_PRODUCER_CHANNEL;
 import static rujche.sample.integration.ChannelNames.LOG_CHANNEL_1;
 import static rujche.sample.integration.ChannelNames.LOG_CHANNEL_2;
@@ -34,7 +40,6 @@ public class IntegrationConfiguration {
         RecipientListRouter router = new RecipientListRouter();
         router.addRecipient(KAFKA_PRODUCER_CHANNEL);
         router.addRecipient(LOG_CHANNEL_1);
-        router.addRecipient(LOG_CHANNEL_2);
         return router;
     }
 
@@ -50,6 +55,21 @@ public class IntegrationConfiguration {
     @ServiceActivator(inputChannel = LOG_CHANNEL_1)
     public MessageHandler logMessageHandler1() {
         return message -> LOGGER.info("logMessageHandler1: Handle message: {}.", message);
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = KAFKA_CONSUMER_CHANNEL, poller = @Poller(fixedDelay = "5000"))
+    public KafkaMessageSource<String, String> source(ConsumerFactory<String, String> consumerFactory)  {
+        ConsumerProperties consumerProperties = new ConsumerProperties(properties.getEventhub());
+        return new KafkaMessageSource<>(consumerFactory, consumerProperties);
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = KAFKA_CONSUMER_CHANNEL)
+    public RecipientListRouter kafkaConsumerChannelRouter() {
+        RecipientListRouter router = new RecipientListRouter();
+        router.addRecipient(LOG_CHANNEL_2);
+        return router;
     }
 
     @Bean
