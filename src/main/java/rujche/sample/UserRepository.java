041286e2dc44c1ab4eee35afc22ee94310a7f38b
rujche <rujche@microsoft.com>
2024-10-21 09:12:24 +0800

    Update sample to use spring-cloud-azure-starter-data-cosmos.

---
+++ src/main/java/rujche/sample/UserRepository.java
@@ -0,0 +1,11 @@
+package rujche.sample;
+
+import com.azure.spring.data.cosmos.repository.ReactiveCosmosRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface UserRepository extends ReactiveCosmosRepository<User, String> {
+
+    Flux<User> findByFirstName(String firstName);
+}
