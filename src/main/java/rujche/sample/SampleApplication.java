454d312988edd0dc98a56ff1601c767320a649d5
rujche <rujche@microsoft.com>
2024-10-29 10:00:02 +0800

    Add message consumer and checkpointer.

--- src/main/java/rujche/sample/SampleApplication.java
+++ src/main/java/rujche/sample/SampleApplication.java
@@ -1,5 +1,7 @@
 package rujche.sample;
 
+import com.azure.spring.messaging.checkpoint.Checkpointer;
+import com.azure.spring.messaging.eventhubs.support.EventHubsHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +10,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
+
+import static com.azure.spring.messaging.AzureHeaders.CHECKPOINTER;
 
 @SpringBootApplication
 public class SampleApplication {
@@ -26,6 +31,23 @@ public class SampleApplication {
 			return MessageBuilder.withPayload("Hello world, " + i++).build();
 		};
 	}
+	@Bean
+	public Consumer<Message<String>> consume() {
+		return message -> {
+			Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(CHECKPOINTER);
+			LOGGER.info("New message received: '{}', partition key: {}, sequence number: {}, offset: {}, enqueued time: {}",
+					message.getPayload(),
+					message.getHeaders().get(EventHubsHeaders.PARTITION_KEY),
+					message.getHeaders().get(EventHubsHeaders.SEQUENCE_NUMBER),
+					message.getHeaders().get(EventHubsHeaders.OFFSET),
+					message.getHeaders().get(EventHubsHeaders.ENQUEUED_TIME)
+			);
 
+			checkpointer.success()
+					.doOnSuccess(success -> LOGGER.info("Message '{}' successfully checkpointed", message.getPayload()))
+					.doOnError(error -> LOGGER.error("Exception found", error))
+					.block();
+		};
+	}
 
 }
